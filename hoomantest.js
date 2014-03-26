0a40a06497ad7a62696d647579c47997369008fa
Gavin Davies <gavindavies@gmail.com>
2014-03-26 15:33:12 +0000

    Get it working with an actual field

--- hoomantest.js
+++ hoomantest.js
@@ -1,13 +1,15 @@
-var noBots = new hOOmanTest($("demo"), {
+var formID = 'hoomans';
+
+var noBots = new hOOmanTest($(formID), {
     callback: function(state) {
         if (state) {
             // success! 
             this.instructions.set("html", "We appreciate it, you can now continue and submit your form.");
 
             // add a field to the form that can show the processor the test has passed. 
-            $("someForm").adopt(new Element("input", {
+            $(formID).adopt(new Element("input", {
                 "type": "hidden",
-                "name": "captcha", // nb I would change this to something unusual. GD.
+                "name": "foobarbaz", // nb I would change this to something unusual. GD.
                 "value": this.mover.name
             }));
 
